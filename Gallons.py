ab33ec787840d1415e36514b373603ea9820f98a
favour384 <desyfavour54@gmail.com>
2023-01-19 17:04:55 +0100

    python assignment

---
+++ Gallons.py
@@ -0,0 +1,5 @@
+inches = input("Enter volume of rainfall in inches ")
+inch = int(inches)
+gallon = inch / 231
+
+print("The volume of water is " , gallon, "gallons")
---
+++ Quiz.py
@@ -0,0 +1,23 @@
+# print start quiz
+# print question1
+# input the question
+# if statement
+# print question2
+# input question2
+# if statement
+
+print("Start quiz")
+print ("question 1")
+quiz = input("Whats the name of our python facilitator? ")
+if quiz == "Miss precious":
+	print ("answer is correct")
+else:
+	print ("The correct answer is miss precious ")
+print("question 2")
+quiz= input("Is 0< 6?")
+if quiz == "True":
+	print("answer is correct")
+else:
+	print("The correct answer is true")
+
+
