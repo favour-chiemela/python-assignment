f34f8f6123a39c896aab2d45fab714de15fccc8e
favour384 <120470710+favour384@users.noreply.github.com>
2023-01-23 08:48:48 +0100

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -6,136 +6,141 @@
 # input question2
 # if statement
 
-print("Start quiz")
-print ("question 1")
-quiz = input("Whats the name of our python facilitator? ")
-if quiz == "Miss precious":
-	print ("answer is correct")
-else:
-	print ("The correct answer is miss precious ")
-print("question 2")
-quiz= input("Is 0< 6?")
-if quiz == "True":
-	print("answer is correct")
-else:
-	print("The correct answer is true")
-print("questiomn 3")
-quiz = input("What is the currency of nigeria")
-if quiz == "Naira"
-	print ("answer is correct")
-else:
-	print ("The correct answer is naira")
-print ("question 4")
-quiz = input("in what part of ur body would u find the circuit ligament")
-if quiz == "Knee"
-	print ("anwer is correct")
-else:
-	pritn("the correct answer is knee")
-print ("question 5")
-quiz = input("whic is the coldest location on earth?")
-if quiz == "East antarctica"
-	print ("anwer is correct")
-else:
-	print("The correct answer is East antarctica")
-print ("question 6")
-quiz = input("which is the highest peak in india?")
-if quiz == "Mount everest"
-	print ("anwer is correct")
-else:
-	print("The correct answer is mount everest")
-print ("question 7")
-quiz = input("the second highest peak in india?")
-if quiz == "Mount k2"
-	print ("anwer is correct")
-else:
-	print("The correct answer is Mount k2")
-print ("question 8")
-quiz = input("Which is the continent with the most number of counrtries?")
-if quiz == "Africa"
-	print ("anwer is correct")
-else:
-	print("The correct answer is Africa")
-print ("question 9")
-quiz = input("Which is the country with the most people?")
-if quiz == "China"
-	print ("anwer is correct")
-else:
-	print("The correct answer is china")
-print ("question 10")
-quiz = input("Which is the fastest animal on earth?")
-if quiz == "Cheetah"
-	print ("anwer is correct")
-else:
-	print("The correct answer is cheetah")
-print ("question 11")
-quiz = input("which is the first biosphere reserve in india?")
-if quiz == "Nilgiri biopshere reserve"
-	print ("anwer is correct")
-else:
-	print("The correct answer is nilgiri biopshere reserve")
-print ("question 12")
-quiz = input("which is the first element on the periodic table of element?")
-if quiz == "Hydrogen"
-	print ("anwer is correct")
-else:
-	print("The correct answer is hydrogen")
-print ("question 13")
-quiz = input("which is the largets state of india?")
-if quiz == "Rajasthan"
-	print ("anwer is correct")
-else:
-	print("The correct answer is Rajasthan")
-print ("question 14")
-quiz = input("which is the hardest substance on earth?")
-if quiz == "Diamond"
-	print ("anwer is correct")
-else:
-	print("The correct answer is diamond")
-print ("question 15")
-quiz = input("which is the national river of india?")
-if quiz == "Gangan"
-	print ("anwer is correct")
-else:
-	print("The correct answer is gangan")
-print ("question 16")
-quiz = input("which is the hottest continient on earth?")
-if quiz == "Africa"
-	print ("anwer is correct")
-else:
-	print("The correct answer is Africa")
-print ("question 17")
-quiz = input("which is the national fruit of india?")
-if quiz == "mango"
-	print ("anwer is correct")
-else:
-	print("The correct answer is mango")
-print ("question 18")
-quiz = input("which is the largest democracy in the world?")
-if quiz == "India"
-	print ("anwer is correct")
-else:
-	print("The correct answer is India")
-print ("question 19")
-quiz = input("which is animal is known as the ship of the desert?")
-if quiz == "Camel"
-	print ("anwer is correct")
-else:
-	print("The correct answer is Camel")
-print ("question 19")
-quiz = input("which is the largets animal in the world?")
-if quiz == "Blue whale"
-	print ("anwer is correct")
-else:
-	print("The correct answer is blue whale")
-print ("question 20")
-quiz = input("which is known as the king of the jungle?")
-if quiz == "lion"
-	print ("anwer is correct")
-else:
-	print("The correct answer is lion")
+user_input = int(input("Enter 1 to start"))
 
+While user_input == 1:
+	
+	print("Start quiz")
+	print ("question 1")
+	quiz = input("Whats the name of our python facilitator? ")
+	if quiz == "Miss precious":
+		print ("answer is correct")
+	else:
+		print ("The correct answer is miss precious ")
+	print("question 2")
+	quiz= input("Is 0< 6?")
+	if quiz == "True":
+		print("answer is correct")
+	else:
+		print("The correct answer is true")
+	print("questiomn 3")
+	quiz = input("What is the currency of nigeria")
+	if quiz == "Naira"
+		print ("answer is correct")
+	else:
+		print ("The correct answer is naira")
+	print ("question 4")
+	quiz = input("in what part of ur body would u find the circuit ligament")
+	if quiz == "Knee"
+		print ("anwer is correct")
+	else:
+		pritn("the correct answer is knee")
+	print ("question 5")
+	quiz = input("whic is the coldest location on earth?")
+	if quiz == "East antarctica"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is East antarctica")
+	print ("question 6")
+	quiz = input("which is the highest peak in india?")
+	if quiz == "Mount everest"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is mount everest")
+	print ("question 7")
+	quiz = input("the second highest peak in india?")
+	if quiz == "Mount k2"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is Mount k2")
+	print ("question 8")
+	quiz = input("Which is the continent with the most number of counrtries?")
+	if quiz == "Africa"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is Africa")
+	print ("question 9")
+	quiz = input("Which is the country with the most people?")
+	if quiz == "China"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is china")
+	print ("question 10")
+	quiz = input("Which is the fastest animal on earth?")
+	if quiz == "Cheetah"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is cheetah")
+	print ("question 11")
+	quiz = input("which is the first biosphere reserve in india?")
+	if quiz == "Nilgiri biopshere reserve"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is nilgiri biopshere reserve")
+	print ("question 12")
+	quiz = input("which is the first element on the periodic table of element?")
+	if quiz == "Hydrogen"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is hydrogen")
+	print ("question 13")
+	quiz = input("which is the largets state of india?")
+	if quiz == "Rajasthan"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is Rajasthan")
+	print ("question 14")
+	quiz = input("which is the hardest substance on earth?")
+	if quiz == "Diamond"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is diamond")
+	print ("question 15")
+	quiz = input("which is the national river of india?")
+	if quiz == "Gangan"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is gangan")
+	print ("question 16")
+	quiz = input("which is the hottest continient on earth?")
+	if quiz == "Africa"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is Africa")
+	print ("question 17")
+	quiz = input("which is the national fruit of india?")
+	if quiz == "mango"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is mango")
+	print ("question 18")
+	quiz = input("which is the largest democracy in the world?")
+	if quiz == "India"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is India")
+	print ("question 19")
+	quiz = input("which is animal is known as the ship of the desert?")
+	if quiz == "Camel"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is Camel")
+	print ("question 19")
+	quiz = input("which is the largets animal in the world?")
+	if quiz == "Blue whale"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is blue whale")
+	print ("question 20")
+	quiz = input("which is known as the king of the jungle?")
+	if quiz == "lion"
+		print ("anwer is correct")
+	else:
+		print("The correct answer is lion")
+		
+	user_input = int(input("Do you wish to continue? yes, press 1 or 0 to exit"))
 
-
+print("Thanks for playing")
 
 
 
